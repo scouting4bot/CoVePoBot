b3fc3ad39fa9f4c0457b254260c37c76f952cda0
effedici <26420747+effedici@users.noreply.github.com>
2021-02-04 10:29:44 +0100

    is possible to disable DB interaction

--- CoVePoBot/application/datasource/initcache.py
+++ CoVePoBot/application/datasource/initcache.py
@@ -9,22 +9,22 @@ def init_vote_session_list(mysql):
     for row in result:
         vote_session_list[row[1]] = {"psw":row[2],"create_date":row[3]}
     print("initialization of cache: ====== Vote Sessions DONE")
-    
+
     for vote_session_key in vote_session_list:
 
         # Select all otps for the 'vote_session_key'
         vote_session_list[vote_session_key]['otps'] = {}
-        result = ExecuteQuerySelect('otp', {'columns2select':'*', 'vote_id':vote_session_key, 'status':'available'}, mysql)
+        result = ExecuteQuerySelect('otp', {'columns2select':'*', 'vote_id':vote_session_key}, mysql)#, 'status':'available'}, mysql)
         for row in result:
             vote_session_list[row[1]]['otps'][row[2]] = row[3]
         print("initialization of cache: ====== OTPs DONE")
-    
+
         # Select all secrets for the 'vote_session_key'
         vote_session_list[vote_session_key]['secrets'] = {}
-        result = ExecuteQuerySelect('secret', {'columns2select':'*', 'vote_id':vote_session_key, 'status':'enabled'}, mysql)
+        result = ExecuteQuerySelect('secret', {'columns2select':'*', 'vote_id':vote_session_key}, mysql)#, 'status':'enabled'}, mysql)
         for row in result:
             vote_session_list[row[1]]['secrets'][row[2]] = row[3]
         print("initialization of cache: ====== Secrets DONE")
-    
+
     print("initialization of cache: ====== Full cache DONE")
     return vote_session_list
